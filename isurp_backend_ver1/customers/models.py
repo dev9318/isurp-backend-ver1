52503f600a8b9622ce5ace37a9ea280bfed7e677
dev9318 <dev.moxaj@gmail.com>
2020-12-18 13:44:06 +0530

    added customer model

--- isurp_backend_ver1/customers/models.py
+++ isurp_backend_ver1/customers/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class Address(models.Model):
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    address1 = models.TextField()
+    address2 = models.TextField()
+    city = models.CharField(max_length=100)
+    postcode = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=100)
+
+
+class Customer(models.Model):
+    id = models.ObjectsIdfield()
+    email = models.EmailField()
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    billing = models.EmbeddedField(
+        model_container = Address
+    )
+    shipping = models.EmbeddedField(
+        model_container = Address
+    )
+    isPayingCustomer = models.BooleanField()
+    ordersCount = models.IntegerField()
+    totalSpent = models.CharField(max_length=100)
+    avatarUrl = models.CharField(max_length=255)
+    guest = models.CharField(max_length=100)
+
