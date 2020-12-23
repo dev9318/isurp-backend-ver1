f18fb1545c08764ac7c1b2cedcabacbedad751e8
dev9318 <dev.moxaj@gmail.com>
2020-12-24 00:59:02 +0530

    Update models.py

--- isurp_backend_ver1/comments/models.py
+++ isurp_backend_ver1/comments/models.py
@@ -8,14 +8,43 @@ class Comment(models.Model):
   int post;
   int parent;
   int author;
-  String authorName;
-  String authorUrl;
-  DateTime date;
-  DateTime dateGmt;
-  Content content;
-  String link;
-  String status;
-  String type;
-  Map<String, String> authorAvatarUrls;
-  List<dynamic> meta;
-  Links links;
+    authorName = models.CharField(max_length=100, default = DEFAULT_VALUE)
+    authorUrl = models.CharField(max_length=100, default = DEFAULT_VALUE)
+    date = models.DateTimeField()
+    dateGmt = models.DateTimeField()
+    content = models.EmbeddedField(model_container = Content)
+    link = models.CharField(max_length=100, default = DEFAULT_VALUE)
+    status = models.CharField(max_length=100, default = DEFAULT_VALUE)
+    type = models.CharField(max_length=100, default = DEFAULT_VALUE)
+    Map<String, String> authorAvatarUrls;
+    # List<dynamic> meta;
+    links = models.EmbeddedField(model_container = Links)
+
+
+class Content(models.Model):
+    rendered = models.CharField(max_length=100, default = DEFAULT_VALUE)
+
+
+class Links(models.Model):
+    self = models.ArrayField(model_container = Collection)
+    collection = models.ArrayField(model_container = Collection)
+    author = models.ArrayField(model_container = Author)
+    up = models.ArrayField(model_container = Up)
+
+
+
+class Author(models.Model):
+    embeddable = models.BooleanField()
+    href = models.CharField(max_length=100, default = DEFAULT_VALUE)
+
+
+
+class Collection(models.Model):
+    href = models.CharField(max_length=100, default = DEFAULT_VALUE)
+
+
+
+class Up(models.Model):
+    embeddable = models.BooleanField()
+    postType = models.CharField(max_length=100, default = DEFAULT_VALUE)
+    href = models.CharField(max_length=100, default = DEFAULT_VALUE)
