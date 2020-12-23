78ec8d45319992e0509ec7625f2faff3e82f988c
dev9318 <dev.moxaj@gmail.com>
2020-12-23 19:03:49 +0530

    Update models.py

--- isurp_backend_ver1/products/models.py
+++ isurp_backend_ver1/products/models.py
@@ -9,57 +9,57 @@ class Product(models.Model):
     name = models.CharField(max_length=100, default = DEFAULT_VALUE)
     slug = models.CharField(max_length=100, default = DEFAULT_VALUE)
     permalink = models.CharField(max_length=100, default = DEFAULT_VALUE)
-  DateTime dateCreated;
-  DateTime dateCreatedGmt;
-  DateTime dateModified;
-  DateTime dateModifiedGmt;
-  String type = models.CharField(max_length=100, default = DEFAULT_VALUE)
-  String status = models.CharField(max_length=100, default = DEFAULT_VALUE)
-  bool featured;
-  String catalogVisibility = models.CharField(max_length=100, default = DEFAULT_VALUE)
-  String description = models.TextField()
-  String shortDescription = models.TextField()
-  String sku = models.CharField(max_length=100, default = DEFAULT_VALUE)
-  String price = models.CharField(max_length=100, default = DEFAULT_VALUE)
-  String regularPrice = models.CharField(max_length=100, default = DEFAULT_VALUE)
-  String salePrice = models.CharField(max_length=100, default = DEFAULT_VALUE)
-  dynamic dateOnSaleFrom;
-  dynamic dateOnSaleFromGmt;
-  dynamic dateOnSaleTo;
-  dynamic dateOnSaleToGmt;
-  String priceHtml = models.CharField(max_length=100, default = DEFAULT_VALUE)
-  bool onSale;
-  bool purchasable;
-  int totalSales;
-  bool virtual;
-  bool downloadable;
-  List<dynamic> downloads;
-  int downloadLimit;
-  int downloadExpiry;
+    dateCreated = models.DateTimeField()
+    dateCreatedGmt = models.DateTimeField()
+    dateModified = models.DateTimeField()
+    dateModifiedGmt = models.DateTimeField()
+    type = models.CharField(max_length=100, default = DEFAULT_VALUE)
+    status = models.CharField(max_length=100, default = DEFAULT_VALUE)
+    featured = models.BooleanField()
+    catalogVisibility = models.CharField(max_length=100, default = DEFAULT_VALUE)
+    description = models.TextField()
+    shortDescription = models.TextField()
+    sku = models.CharField(max_length=100, default = DEFAULT_VALUE)
+    price = models.CharField(max_length=100, default = DEFAULT_VALUE)
+    regularPrice = models.CharField(max_length=100, default = DEFAULT_VALUE)
+    salePrice = models.CharField(max_length=100, default = DEFAULT_VALUE)
+    dateOnSaleFrom = models.DateTimeField()
+    dateOnSaleFromGmt = models.DateTimeField()
+    dateOnSaleTo = models.DateTimeField()
+    dateOnSaleToGmt = models.DateTimeField()
+    priceHtml = models.CharField(max_length=100, default = DEFAULT_VALUE)
+    onSale = models.BooleanField()
+    purchasable = models.BooleanField()
+    totalSales = models.IntegerField()
+    virtual = models.BooleanField()
+    #     downloadable = models.BooleanField()
+    #   List<dynamic> downloads;
+    #     downloadLimit = models.IntegerField()
+    #     downloadExpiry = models.IntegerField()
   String externalUrl = models.CharField(max_length=100, default = DEFAULT_VALUE)
   String buttonText = models.CharField(max_length=100, default = DEFAULT_VALUE)
   String taxStatus = models.CharField(max_length=100, default = DEFAULT_VALUE)
   String taxClass = models.CharField(max_length=100, default = DEFAULT_VALUE)
-  bool manageStock;
-  int stockQuantity;
+  bool manageStock = models.BooleanField()
+  int stockQuantity = models.IntegerField()
   String stockStatus = models.CharField(max_length=100, default = DEFAULT_VALUE)
   String backOrders = models.CharField(max_length=100, default = DEFAULT_VALUE)
-  bool backordersAllowed;
-  bool backordered;
-  bool soldIndividually;
+  bool backordersAllowed = models.BooleanField()
+  bool backordered = models.BooleanField()
+  bool soldIndividually = models.BooleanField()
   String weight = models.CharField(max_length=100, default = DEFAULT_VALUE)
   Dimensions dimensions;
-  bool shippingRequired;
-  bool shippingTaxable;
+  bool shippingRequired = models.BooleanField()
+  bool shippingTaxable = models.BooleanField()
   String shippingClass = models.CharField(max_length=100, default = DEFAULT_VALUE)
-  int shippingClassId;
-  bool reviewsAllowed;
+  int shippingClassId = models.IntegerField()
+  bool reviewsAllowed = models.BooleanField()
   String averageRating = models.CharField(max_length=100, default = DEFAULT_VALUE)
-  int ratingCount;
+  int ratingCount = models.IntegerField()
   List<int> relatedIds;
   List<dynamic> upsellIds;
   List<dynamic> crossSellIds;
-  int parentId;
+  int parentId = models.IntegerField()
   String purchaseNote = models.CharField(max_length=100, default = DEFAULT_VALUE)
   List<ProductCategory> categories;
   List<dynamic> tags;
@@ -68,10 +68,10 @@ class Product(models.Model):
   List<DefaultAttribute> defaultAttributes;
   List<dynamic> variations;
 #   List<dynamic> groupedProducts;
-  int menuOrder;
+  int menuOrder = models.IntegerField()
 #   List<dynamic> metaData;
   Links links;
-  int decimals;
+  int decimals = models.IntegerField()
   String vendor = models.CharField(max_length=100, default = DEFAULT_VALUE)
 
 
@@ -82,14 +82,18 @@ class Dimensions(models.Model):
     height = models.CharField(max_length=100, default = DEFAULT_VALUE)
 
 class ProductImage(models.Model):
-    #   int id;
+    int id;
     productId = models.CharField(max_length=100)
-    DateTime dateCreated;
-    DateTime dateCreatedGmt;
-    DateTime dateModified;
-    DateTime dateModifiedGmt;
+    DateTime dateCreated = models.DateTimeField()
+    DateTime dateCreatedGmt = models.DateTimeField()
+    DateTime dateModified = models.DateTimeField()
+    DateTime dateModifiedGmt = models.DateTimeField()
     src = models.CharField(max_length=100, default = DEFAULT_VALUE)
     name = models.CharField(max_length=100, default = DEFAULT_VALUE)
 #   String alt;
 
 
+class ProductCategory(models.Model):
+    int id;
+    String name = models.CharField(max_length=100, default = DEFAULT_VALUE)
+    slug = models.CharField(max_length=100, default = DEFAULT_VALUE)
