6144581a9c04eb7186dbcabd225b4242621150d5
dev9318 <dev.moxaj@gmail.com>
2020-12-24 12:57:21 +0530

    Update models.py

--- isurp_backend_ver1/comments/models.py
+++ isurp_backend_ver1/comments/models.py
@@ -1,6 +1,36 @@
 from djongo import models
 
+
+
+DEFAULT_VALUE = ''
+
 # Create your models here.
+
+
+class Content(models.Model):
+    rendered = models.CharField(max_length=100, default = DEFAULT_VALUE)
+
+
+class Collection(models.Model):
+    href = models.CharField(max_length=100, default = DEFAULT_VALUE)
+
+class Author(models.Model):
+    embeddable = models.BooleanField()
+    href = models.CharField(max_length=100, default = DEFAULT_VALUE)
+
+
+class Up(models.Model):
+    embeddable = models.BooleanField()
+    postType = models.CharField(max_length=100, default = DEFAULT_VALUE)
+    href = models.CharField(max_length=100, default = DEFAULT_VALUE)
+
+
+class Links(models.Model):
+    self = models.ArrayField(model_container = Collection)
+    collection = models.ArrayField(model_container = Collection)
+    author = models.ArrayField(model_container = Author)
+    up = models.ArrayField(model_container = Up)
+
 
 
 class Comment(models.Model):
@@ -16,35 +46,6 @@ class Comment(models.Model):
     link = models.CharField(max_length=100, default = DEFAULT_VALUE)
     status = models.CharField(max_length=100, default = DEFAULT_VALUE)
     type = models.CharField(max_length=100, default = DEFAULT_VALUE)
-    Map<String, String> authorAvatarUrls;
+    # Map<String, String> authorAvatarUrls;
     # List<dynamic> meta;
     links = models.EmbeddedField(model_container = Links)
-
-
-class Content(models.Model):
-    rendered = models.CharField(max_length=100, default = DEFAULT_VALUE)
-
-
-class Links(models.Model):
-    self = models.ArrayField(model_container = Collection)
-    collection = models.ArrayField(model_container = Collection)
-    author = models.ArrayField(model_container = Author)
-    up = models.ArrayField(model_container = Up)
-
-
-
-class Author(models.Model):
-    embeddable = models.BooleanField()
-    href = models.CharField(max_length=100, default = DEFAULT_VALUE)
-
-
-
-class Collection(models.Model):
-    href = models.CharField(max_length=100, default = DEFAULT_VALUE)
-
-
-
-class Up(models.Model):
-    embeddable = models.BooleanField()
-    postType = models.CharField(max_length=100, default = DEFAULT_VALUE)
-    href = models.CharField(max_length=100, default = DEFAULT_VALUE)
