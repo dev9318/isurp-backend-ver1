1f5cf7a4dfdade9be60a98afb076722011b41ea5
dev9318 <dev.moxaj@gmail.com>
2020-12-26 23:18:26 +0530

    added get cart

--- isurp_backend_ver1/carts/views.py
+++ isurp_backend_ver1/carts/views.py
@@ -19,13 +19,11 @@ class get_cart(APIView):
         try:
             cart_query = CartModel.objects.get(uid = uid)
             data = {
-                    cartContents: json["cartContents"] == null ? null : new List<CartContent>.from(json["cartContents"].map((x) => CartContent.fromJson(x))),
-                    cartNonce: json["cart_nonce"] == null ? null : json["cart_nonce"],
-                    cartTotals: json["cart_totals"] == null ? null : CartTotals.fromJson(json["cart_totals"]),
-                    points: json["points"] == null ? null : Points.fromJson(json["points"]),
-                    purchasePoint: json["purchase_point"] == null ? null : json["purchase_point"],
-                    currency: json["currency"] == null ? 'USD' : json["currency"],
-                    cartFees: json["cart_fees"] == null ? null : List<CartFee>.from(json["cart_fees"].map((x) => CartFee.fromJson(x))),
+                    'cartContents': cart_query.cartContents,
+                    'cartNonce': cart_query.cartNonce,
+                    'cartTotals': cart_query.cartTotals,
+                    'currency': cart_query.currency,
+                    'cartFees': cart_query.cartFees,
                 }
         except:
             data = {'status':'No user'}
