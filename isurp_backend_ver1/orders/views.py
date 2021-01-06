a1302841ac3b31f112e123dc9e2eab48b1ac3542
dev9318 <dev.moxaj@gmail.com>
2021-01-06 21:21:31 +0530

    Update views.py

--- isurp_backend_ver1/orders/views.py
+++ isurp_backend_ver1/orders/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from .models import ReviewModel
+import uuid
+from datetime import datetime
+
+
 
 # Create your views here.
+
+
+class post_order(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def post(self, request):
+
+        
