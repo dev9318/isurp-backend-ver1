69aba02b14b34e0de4e0fd1ad7a9f15c6103b6ad
dev9318 <dev.moxaj@gmail.com>
2021-01-12 17:05:20 +0530

    Update views.py

--- isurp_backend_ver1/orders/views.py
+++ isurp_backend_ver1/orders/views.py
@@ -5,10 +5,13 @@ from rest_framework import authentication, permissions
 from .models import ReviewModel
 import uuid
 from datetime import datetime
+import razorpay
 
 
 
 # Create your views here.
+keyId = ''
+keySecret = ''
 
 
 class post_order(APIView):
@@ -16,5 +19,19 @@ class post_order(APIView):
     permission_classes = []
 
     def post(self, request):
+
+        order_id = request.data.get('order_id')
+        razorpay_payment_id = request.data.get('razorpay_payment_id')
+        razorpay_signature = request.data.get('razorpay_signature')
+
+        client = razorpay.Client(auth = (keyId, keySecret))
+        params_dict = {
+            'order_id': order_id,
+            'razorpay_payment_id': razorpay_payment_id,
+            'razorpay_signature': razorpay_signature
+        }
+
+        client.utility.verify_payment_signature(params_dict)
+
 
         
