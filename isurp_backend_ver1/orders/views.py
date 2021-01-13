e9a2bb0c4515ac89b1ed982a684b1b99ad3a8b68
dev9318 <dev.moxaj@gmail.com>
2021-01-13 18:44:39 +0530

    Update views.py

--- isurp_backend_ver1/orders/views.py
+++ isurp_backend_ver1/orders/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
-from .models import Order
+from .models import Order, Address
 import uuid
 from datetime import datetime
+from isurp_backend_ver1.carts.models import CartModel, CartContent
 import razorpay
 
 
@@ -42,15 +43,31 @@ class return_order(APIView):
     def post(self, request):
         uid = request.data.get('uid')
 
-        customer_query = Order.objects.create()
-        customer_query.billing.firstName = request.data.get('billing_first_name')
-        customer_query.billing.lastName = request.data.get('billing_last_name')
-        customer_query.billing.address1 = request.data.get('billing_address_1')
-        customer_query.billing.city = request.data.get('billing_city')
-        customer_query.billing.postcode = request.data.get('billing_postcode')
-        customer_query.billing.country = request.data.get('billing_country')
-        customer_query.billing.state = request.data.get('billing_state')
-        customer_query.billing.email = request.data.get('billing_email')
-        customer_query.billing.phone = request.data.get('billing_phone')
+        cart_query = CartModel.objects.get(uid = uid)
+
+        client = razorpay.Client(auth = (keyId, keySecret))
+        order_amount = cart_query.cartTotals.total
+        order_currency = 'INR'
+        # order_receipt = 'order_rcptid_11'
+        notes = {'uid': uid}
+
+        order_id = client.order.create(amount=order_amount, currency=order_currency, notes=notes)
+        
+
+        customer_query = Order.objects.create( 
+            billing = Address(
+                firstName = request.data.get('billing_first_name'),
+                lastName = request.data.get('billing_last_name'),
+                address1 = request.data.get('billing_address_1'),
+                city = request.data.get('billing_city'),
+                postcode = request.data.get('billing_postcode'),
+                country = request.data.get('billing_country'),
+                state = request.data.get('billing_state'),
+                email = request.data.get('billing_email'),
+                phone = request.data.get('billing_phone')
+                ),
+            orderId = order_id,
+            cartModel = cart_query,   
+        )
 
         
