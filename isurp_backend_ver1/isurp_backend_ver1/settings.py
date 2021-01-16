85e67675483830220e721dde73ce056ba4ba5a3e
dev9318 <dev.moxaj@gmail.com>
2021-01-16 22:27:39 +0530

    Update settings.py

--- isurp_backend_ver1/isurp_backend_ver1/settings.py
+++ isurp_backend_ver1/isurp_backend_ver1/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'carts',
+    'comments',
     'customers',
+    'orders',
+    'products',
 ]
 
 MIDDLEWARE = [
