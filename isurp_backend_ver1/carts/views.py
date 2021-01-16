b29b04fcf4f2b1b83e30764e0a561aaabe7c794b
dev9318 <dev.moxaj@gmail.com>
2021-01-16 22:21:06 +0530

    fixed typo

--- isurp_backend_ver1/carts/views.py
+++ isurp_backend_ver1/carts/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
 from .models import CartModel, CartContent
-from isurp_backend_ver1.products.models import Product
+from products.models import Product
 import uuid
 
 # Create your views here.
